592808cdb8bb1c02c02f6529f5f1ddccbe0dbcbb
guoxiongLuo <guoxiong.luo@ectrip.com>
2019-02-22 13:57:52 +0800

    上传文件

---
+++ www/web/easyMarket/precache-manifest.d8e132b3c0dfb27a2ccde3a486e76ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea4313b7e13a69f9e222",
+    "url": "/web/pero_LuoNideH5/static/js/runtime~main.ea4313b7.js"
+  },
+  {
+    "revision": "dd3f15441ff7bf81e56f",
+    "url": "/web/pero_LuoNideH5/static/js/main.dd3f1544.chunk.js"
+  },
+  {
+    "revision": "6bdd363146f5d74d793e",
+    "url": "/web/pero_LuoNideH5/static/js/1.6bdd3631.chunk.js"
+  },
+  {
+    "revision": "dd3f15441ff7bf81e56f",
+    "url": "/web/pero_LuoNideH5/static/css/main.175c0c5b.chunk.css"
+  },
+  {
+    "revision": "6bdd363146f5d74d793e",
+    "url": "/web/pero_LuoNideH5/static/css/1.24e542a4.chunk.css"
+  },
+  {
+    "revision": "2f02687a6eee652e7ab268c9d6f89855",
+    "url": "/web/pero_LuoNideH5/index.html"
+  }
+];
