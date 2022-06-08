b7a03025e0b185061828601587e74281b94e3936
罗国雄 <1025558554@qq.com>
2022-06-08 17:57:56 +0800

    feat: test logs

--- script/release.sh
+++ script/release.sh
@@ -1,17 +1,40 @@
-#!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
 
-# 提交编译后的文件
-git add -A
-git commit -m "chore(build): 打包编译"
+# 读取当前分支
+branch=`git symbolic-ref --short HEAD`
 
-# 生成 CHANGELOG.md，修改版本号，打上版本号的 tag
-npm run version
+if [ "$branch" = "master" ]; then
+  # 编译
+  yarn build
 
-# 发布
-git push
+  # 提交编译后的文件
+  git add -A
+  git commit -m "chore(build): 打包编译"
 
-# 提交所有 tag
-git push --tags
+  # 生成 CHANGELOG.md，修改版本号，打上版本号的 tag
+  yarn standard
+
+  # 发布
+  git push
+
+  # 提交所有 tag
+  git push --tags
+
+elif  [ "$branch" = "dev" ]; then
+  # dev分支不打tag，也不生成CHANGELOG.md
+
+  # 编译
+  yarn build
+
+  # 提交编译后的文件
+  git add -A
+  git commit -m "chore(release): dev"
+
+  # 上传远程仓库
+  git push origin dev
+
+else
+  # 其他分支直接退出
+  echo -e "\033[31m 只能在 master 或者 dev 分支上执行 yarn release！ \033[0m"
+fi
---
+++ test.md
@@ -0,0 +1,1 @@
+npm run version  -- --prerelease alpha
