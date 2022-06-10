ae57978001f3b7d7760cb0510e6baa7fa3bb8972
罗国雄 <1025558554@qq.com>
2022-06-10 15:19:33 +0800

    feat: test

---
+++ script/install.sh
@@ -0,0 +1,5 @@
+# 确保脚本抛出遇到的错误
+set -e
+npm install
+
+npm run bootstrap
--- script/release.sh
+++ script/release.sh
@@ -6,7 +6,7 @@ branch=`git symbolic-ref --short HEAD`
 
 if [ "$branch" = "main" ]; then
  
-  npm run test3
+  npm run version
 
   git push origin main
   
