645f93538570412c036442fe424dc6e1936440d6
罗国雄 <1025558554@qq.com>
2022-08-10 17:22:56 +0800

    feat: update master code

--- src/admin/logic/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = class extends think.Logic {
-  loginAction() {
-    this.allowMethods = 'post';
-    this.rules = {
-      username: { required: true, string: true },
-      password: { required: true, string: true }
-    };
-  }
-};
--- src/common/config/extend.js
+++
@@ -1,7 +0,0 @@
-const model = require('think-model');
-const cache = require('think-cache');
-
-module.exports = [
-  model(think.app),
-  cache
-];
