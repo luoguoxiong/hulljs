f17903826845a60a90979f997bc58e730ea92b94
罗国雄 <1025558554@qq.com>
2022-08-31 17:43:28 +0800

    fix: update release branch

--- script/release.sh
+++ script/release.sh
@@ -2,9 +2,9 @@
 set -e
 
 # 读取当前分支
-# branch=`git symbolic-ref --short HEAD`
+branch=`git symbolic-ref --short HEAD`
 
-# if [ "$branch" = "master" ]; then
+if [ "$branch" = "master" ]; then
  
   npm run version
 
@@ -12,7 +12,7 @@ set -e
   
   git push --follow-tags origin main
 
-# else
+else
 
-#   echo -e "\033[31m 只能在 master 或者 dev 分支上执行 yarn release！ \033[0m"
-# fi
+  echo -e "\033[31m 只能在 master 或者 dev 分支上执行 yarn release！ \033[0m"
+fi
